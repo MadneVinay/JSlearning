31029816e0e8128cbea26e3cd5968d90d775bcca
MadneVinay <vinaymadne30@gmail.com>
2023-07-19 16:37:23 +0530

    Added variables and datatypes
    Added variables and datatypes

---
+++ 01_variableAssign.js
@@ -0,0 +1,33 @@
+
+//1
+
+var myFullName;
+console.log(myFullName);
+myFullName="Vinay Madne";
+console.log(myFullName);
+myFullName="Vinay GajananRao Madne";
+console.log("My Full Name is:-"+myFullName);
+
+
+//2
+
+var mylove="I Love only Js";
+console.log(mylove);
+
+
+//3
+
+var firstName="Vinay"
+var lastName="Madne"
+var age="23"
+var adress="Amravati"
+var pincode="444720"
+var gender="Male"
+var isMarried="No"
+console.log("My First Name is:-"+firstName);
+console.log("My Last Name is:-"+lastName);
+console.log("My Age is:-"+age);
+console.log("Current Address:-"+adress);
+console.log("Pincode:-"+pincode);
+console.log("Gender:-"+gender);
+console.log("Is Married:-"+isMarried);
---
+++ 02_typeOf.js
@@ -0,0 +1,35 @@
+//1
+
+var bankname
+console.log(bankname);
+console.log(typeof bankname);
+bankname="SBI Bank"; 
+console.log(bankname);
+console.log(typeof bankname);
+
+//2
+
+var marks='90%'
+console.log(marks);
+console.log(typeof marks);
+
+
+
+//3
+
+var isWorking
+console.log(isWorking);
+console.log(typeof isWorking);
+isWorking=true
+console.log(isWorking);
+console.log(typeof isWorking);
+
+
+//4
+
+var totalCount="Hundred And Seven"
+console.log(totalCount);
+console.log(typeof totalCount);
+totalCount=107
+console.log(totalCount);
+console.log(typeof totalCount);
