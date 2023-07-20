c345c497fedffb9369c49837fbeb336f68e1645a
MadneVinay <vinaymadne30@gmail.com>
2023-07-20 10:04:50 +0530

    Swapped two character values using temp character
    Swapped two character values using temp character

---
+++ swap.js
@@ -0,0 +1,20 @@
+var sweety= "Sweety"
+var cutie = "Cutie"
+console.log("Before swapping", ' Sweety=', sweety, 'Cutie=', cutie);
+var Temp = sweety
+sweety = cutie
+cutie = Temp
+console.log("After swapping", 'Sweety =', sweety, 'Cutie=', cutie);
+
+console.log("*******************************");
+//2
+
+var num1 = 100
+var num2 = 200
+num3 = 300
+console.log("Before swapping", ' NUM1=', num1, 'NUM2=', num2, 'NUM3=', num3);
+var Temp = num1
+num1 = num2
+num2 = num3
+num3 = Temp
+console.log("After swapping", ' NUM1=', num1, 'NUM2=', num2, 'NUM3=', num3);
