e0145f976c897fc080f80c4ae6d2334781ad0035
MadneVinay <vinaymadne30@gmail.com>
2023-07-31 18:48:25 +0530

    Learned for and while loop
    Learned for and while loop

---
+++ 06_forAssingment01.js
@@ -0,0 +1,28 @@
+console.log(`"Program to Print Numbers from 5 to 15 increamenting by 1"`);
+for (let index = 5; index <= 15; index++) {
+    console.log(`${index}`);   
+}
+console.log(`"Program to Print Numbers from 50 to 40 decreamenting by 1"`);
+for (let index = 50; index >= 40; index--) {
+    console.log(`${index}`);   
+}
+console.log(`"Program to Print First 15 odd Numbers"`);
+for (let index = 1; index <= 30; index = index+2) {
+    console.log(`${index}`);   
+}
+console.log(`"Program to Print First 10 Even Numbers"`);
+for (let index = 2; index <= 20; index = index+2) {
+    console.log(`${index}`);   
+}
+console.log(`"Program to Print Table of 5"`);
+for (let index = 5; index <= 50; index = index+5) {
+    console.log(`${index}`);   
+}
+console.log(`"Program to Print Table of 100"`);
+for (let index = 10; index <= 100; index = index+10) {
+    console.log(`${index}`);   
+}
+console.log(`"Program to Print Table of 100 in reverse"`);
+for (let index = 100; index >= 10; index = index-10) {
+    console.log(`${index}`);   
+}
---
+++ 06_whileAssignment01.js
@@ -0,0 +1,42 @@
+console.log(`"Program to Print Numbers from 5 to 15 increamenting by 1"`);
+i = 5
+while (i <= 15) {
+    console.log(`${i}`);
+    i++
+}
+console.log(`"Program to Print Numbers from 50 to 40 decreamenting by 1"`);
+i = 50
+while (i >= 40) {
+    console.log(`${i}`);
+    i--
+}
+console.log(`"Program to Print First 15 odd Numbers"`);
+i = 1
+while (i <= 30) {
+    console.log(`${i}`);
+    i = i+2   
+}
+console.log(`"Program to Print First 10 Even Numbers"`);
+i = 2
+while (i <= 20) {
+    console.log(`${i}`);
+    i = i+2    
+}
+console.log(`"Program to Print Table of 5"`);
+i = 5
+while (i <= 50) {
+    console.log(`${i}`);
+    i = i+5   
+}
+console.log(`"Program to Print Table of 10"`);
+i = 10
+while (i <= 100) {
+    console.log(`${i}`);
+    i = i+10   
+}
+console.log(`"Program to Print Table of 10 in reverse"`);
+i = 100
+while (i >= 10) {
+    console.log(`${i}`);
+    i = i-10   
+}
